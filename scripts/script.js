4381214f5ef82a9a16dc029e44565838f9a02c72
Kyle Rush <kdr213@gmail.com>
2017-11-11 15:21:40 -0800

    api call returning 200, X-origin working

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,33 @@ $(document).ready(function() {
 
 function registerClickEvents() {
 	$("#login-form-submit").click(function() {
+		
+		var logindata = {};
+		logindata['username'] = $("#username-input").val();
+		logindata['password'] = md5($("#password-input").val());		
+		$("#debug-div").append(logindata.username + " " + logindata.password + "<br>");
 
-		var formdata = $("#login-form").serialize();
-		$("#debug-div").text(formdata);
 
+		$.ajax( 
+        {
+          method: "POST",
+          url: "https://ips3y5tbeh.execute-api.us-west-2.amazonaws.com/prod/larrys-login",
+          dataType: "json",
+          data: JSON.stringify(logindata),
+          crossdomain: true,
+          async:true, 
+          success: function(data) {
+            //Success callback of API call
+            alert("SUCCESS" + data);
+          },
+          error: function(data) {
+            //Error callback of API call
+            alert("ERROR" + JSON.stringify(data));
+          }
 
-		//$.ajax({});
+        }
+      ).done(function(data) {
+        alert(data);
+      });
 	});
 }
