abd372a1ae30388bebc7c84bb30b4f6e038703a3
Kyle Rush <kdr213@gmail.com>
2017-11-11 22:31:49 -0800

    testing login, register

--- scripts/script.js
+++ scripts/script.js
@@ -12,27 +12,55 @@ function registerClickEvents() {
 		logindata['password'] = $("#password-input").val();		
 		$("#debug-div").append(logindata.username + " " + logindata.password + "<br>");
 
-
+    //Make API call to login lambda function.
 		$.ajax( 
         {
           method: "POST",
-          url: "https://ips3y5tbeh.execute-api.us-west-2.amazonaws.com/prod/larrys-login",
+          url: "https://87uo5r92ya.execute-api.us-west-2.amazonaws.com/prod/login",
           dataType: "json",
           data: JSON.stringify(logindata),
           crossdomain: true,
           async:true, 
           success: function(data) {
             //Success callback of API call
-            alert("SUCCESS" + data);
+            console.log("SUCCESS " + data);
           },
           error: function(data) {
             //Error callback of API call
-            alert("ERROR" + JSON.stringify(data));
+            console.log("ERROR " + data);
           }
 
         }
-      ).done(function(data) {
-        alert(data);
-      });
+      );
 	});
+
+
+  $("#register-form-submit").click(function() {
+    
+    var logindata = {};
+    logindata['username'] = $("#username-input").val();
+    logindata['password'] = $("#password-input").val();   
+    $("#debug-div").append(logindata.username + " " + logindata.password + "<br>");
+
+
+    $.ajax( 
+        {
+          method: "POST",
+          url: "https://87uo5r92ya.execute-api.us-west-2.amazonaws.com/prod/register-user",
+          dataType: "json",
+          data: JSON.stringify(logindata),
+          crossdomain: true,
+          async:true, 
+          success: function(data) {
+            //Success callback of API call
+            console.log("SUCCESS " + data);
+          },
+          error: function(data) {
+            //Error callback of API call
+            console.log("ERROR " + data);
+          }
+
+        }
+      );
+  });
 }
